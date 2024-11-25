f8f3964f0b8a20d20447e445d601d398839921e6
gabriel.mango <gabriel.mango@defensoria.mg.def.br>
2024-11-25 16:10:33 -0300

    feat: add schemas to validate data input and output

--- app/schemas.py
+++ app/schemas.py
@@ -1,1 +1,23 @@
 """Validation Schemes (Pydantic)"""
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr
+
+
+class UserCreate(BaseModel):
+    name: str
+    email: EmailStr
+    password: str
+    permission_level: Optional[str] = 'user'
+
+
+class UserResponse(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+    permission_level: str
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
