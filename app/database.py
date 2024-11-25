0c0ae429d4d5cb2861344258713faed00dc56d81
gabriel.mango <gabriel.mango@defensoria.mg.def.br>
2024-11-25 15:52:51 -0300

    feat: creating database settings

--- app/database.py
+++ app/database.py
@@ -1,1 +1,27 @@
 """SQLite Database Configuration"""
+
+from dotenv import dotenv_values
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+config = dotenv_values('.env')
+
+DATABASE_NAME = config['DATABASE_URL']
+
+engine = create_engine(
+    DATABASE_NAME, connect_args={'check_same_thread': False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    """Return the database"""
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
