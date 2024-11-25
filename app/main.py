40734747ef62d5f8600165fe7645932d09499686
gabriel.mango <gabriel.mango@defensoria.mg.def.br>
2024-11-25 16:00:26 -0300

    feat: creation of the root endpoint and initial testing of the project

--- app/main.py
+++ app/main.py
@@ -1,1 +1,17 @@
 """Main file to initialize the app"""
+from fastapi import FastAPI
+
+from app.database import Base, engine
+from app.routers import users
+
+app = FastAPI()
+
+Base.metadata.create_all(bind=engine)
+
+app.include_router(users.router)
+
+
+@app.get('/')
+def root():
+    """Default endpoint for the API"""
+    return {'message': 'Welcome to the FastAPI User API'}
